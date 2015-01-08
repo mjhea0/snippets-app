2da0dc1ae095599052a0afe7b7fc674aebf2e76b
Michael Herman <hermanmu@gmail.com>
2015-01-08 16:04:00 -0700

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -9,11 +9,13 @@ logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 # Connect to the database from python
 logging.debug("Connecting to PostgreSQL")
-connection = psycopg2.connect("dbname='snippets' user='action' host='localhost'")
+# connection = psycopg2.connect("dbname='snippets' user='action' host='localhost'")
+connection = psycopg2.connect("dbname='snippets' host='localhost'")
 logging.debug("Database connection established")
 
+
 def put(name, snippet, hide=False):
-    """Store a snippet with an associated name.""" 
+    """Store a snippet with an associated name."""
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
     with connection, connection.cursor() as cursor:
         try:
@@ -25,6 +27,7 @@ def put(name, snippet, hide=False):
     logging.debug("Snippet stored successfully.")
     return name, snippet
 
+
 def catalog():
     """Query the available keywords from the snippets table."""
     logging.info("Querying the database")
@@ -34,7 +37,8 @@ def catalog():
         for row in rows:
             print row['keyword']
     logging.debug("Query complete")
-    
+
+
 def search(string):
     """Return a list of snippets containing a given string"""
     logging.info("Searching snippets for {!r}".format(string))
@@ -45,9 +49,8 @@ def search(string):
         for row in rows:
             print row['message']
     logging.debug("Search complete")
-    
-    
-    
+
+
 def get(name):
     """Retrieve the snippet with a given name."""
     logging.info("Retrieving snippet {!r}".format(name))
@@ -66,42 +69,43 @@ def get(name):
     else:
         return row[0]
 
+
 def main():
-    """Main function"""""
+    """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    
+
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
-    
+
     # Subparser for the put command
     logging.debug("Constructing put subparser")
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="The name of the snippet")
     put_parser.add_argument("snippet", help="The snippet text")
     put_parser.add_argument("--hide", help="Sets the hidden column to True", action="store_true")
-    
+
     # Subparser for the catalog command
     logging.debug("Constructing catalog subparser")
     catalog_parser = subparsers.add_parser("catalog", help="Query snippet keywords")
-    
+
     # Subparser for the search command
     logging.debug("Constructing search subparser")
     search_parser = subparsers.add_parser("search", help="Search snippets for a string")
     search_parser.add_argument("string", help="The string you are searching for")
-    
+
     # Subparser for the get command
     logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="The name of the snippet")
-    
+
     arguments = parser.parse_args(sys.argv[1:])
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
     command = arguments.pop("command")
-    
+
     if command == "put":
-        name, snippet, hide = put(**arguments)
-        print("Stored {!r} as {!r}. Hidden is set to {!r}.".format(snippet, name, hide))
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}.".format(snippet, name))
     elif command == "get":
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
@@ -109,10 +113,9 @@ def main():
         keywords = catalog()
         print("Retrieved keywords")
     elif command == "search":
-        word = search(**arguments)
+        search(**arguments)
+        print
         print("Search complete")
-        #print(Found {!r} in these messages".format(word))
-        #why cant i get this to print?
+        print("Found {!r} in these messages".format(sys.argv[2]))
 if __name__ == "__main__":
     main()
-
